43620f0d19037d4c7b8d504effe57ddc4b5ce71e
konainbaig78 <konainbaig915@gmail.com>
2024-08-21 23:30:19 +0500

    done

---
+++ app.js
@@ -0,0 +1,6 @@
+function bulb_on(){
+    document.getElementById('bulb').src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAVwmyv14WApdNKcARm8ZoqcTHPGXUSe1dkgR4ElavX-7sjVnB_EpH2gK8QFJ5Lt8pdh0&usqp=CAU';
+}
+function bulb_off(){
+    document.getElementById('bulb').src='https://i.pinimg.com/564x/e0/fd/25/e0fd25f9127a9a109a0648c83ee61643.jpg';
+}
